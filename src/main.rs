322c2607b605323960a66af0c5a8235ae35b85d8
nobel-von-it <maksimdavydenko12@gmail.com>
2025-06-12 15:10:05 +0700

    Start developing templates

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use clap::{Arg, ArgAction, Command};
+use std::process;
+
+use clap::{Arg, ArgAction, ArgMatches, Command};
 
 #[derive(Debug, Default, Clone)]
 struct ArgManager {
@@ -46,15 +48,96 @@ impl SubcommandManager {
             .about("Cargo API wrapper")
             .arg(am.form_arg_vec("commands", true, "The commands to run with Cargo"))
     }
+    fn template_command(mut am: ArgManager) -> Command {
+        Command::new("template")
+            .about("Template for a new project")
+            .arg(am.form_arg_flags("name", true, "The name of the project"))
+            .arg(am.form_arg_flags("path", true, "The path to the template"))
+    }
 }
 
 fn command() -> Command {
     let am = ArgManager::default();
-    Command::new("crgl").subcommand(SubcommandManager::cargo_command(am.clone()))
+    Command::new("crgl")
+        .subcommand(SubcommandManager::cargo_command(am.clone()))
+        .subcommand(SubcommandManager::template_command(am.clone()))
+}
+
+trait Parser {
+    fn parse_args(matches: &ArgMatches) -> Self;
+}
+
+#[derive(Debug)]
+enum CrglCommand {
+    Cargo(CargoCommand),
+    Template(TemplateCommand),
+}
+
+impl Parser for CrglCommand {
+    fn parse_args(matches: &ArgMatches) -> Self {
+        match matches.subcommand() {
+            Some(("cargo", sub_matches)) => {
+                CrglCommand::Cargo(CargoCommand::parse_args(sub_matches))
+            }
+            Some(("template", sub_matches)) => {
+                CrglCommand::Template(TemplateCommand::parse_args(sub_matches))
+            }
+            _ => todo!(),
+        }
+    }
+}
+
+impl CrglCommand {
+    fn execute(&self) {
+        match self {
+            CrglCommand::Cargo(c) => {
+                let mut cargo_command = process::Command::new("cargo");
+                for command in &c.commands {
+                    cargo_command.arg(command);
+                }
+                cargo_command.spawn().unwrap().wait().unwrap();
+            }
+            CrglCommand::Template(t) => println!("{:#?}", t),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct CargoCommand {
+    commands: Vec<String>,
+}
+
+impl Parser for CargoCommand {
+    fn parse_args(matches: &ArgMatches) -> Self {
+        let commands = matches
+            .get_many::<String>("commands")
+            .unwrap()
+            .cloned()
+            .collect();
+        Self { commands }
+    }
+}
+
+#[derive(Debug)]
+struct TemplateCommand {
+    name: String,
+    path: String,
+}
+
+impl Parser for TemplateCommand {
+    fn parse_args(matches: &ArgMatches) -> Self {
+        let name = matches.get_one::<String>("name").unwrap();
+        let path = matches.get_one::<String>("path").unwrap();
+        Self {
+            name: name.clone(),
+            path: path.clone(),
+        }
+    }
 }
 
 fn main() {
     let matches = command().get_matches();
 
-    println!("{:#?}", matches);
+    let command = CrglCommand::parse_args(&matches);
+    command.execute();
 }
